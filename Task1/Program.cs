6a8bd90c9a475f463d28ccc4358b5e429f2d4e61
Aryna Shkolna <kn223_shal@student.ztu.edu.ua>
2022-11-12 12:26:00 +0200

    Зміна тексту в трьох перших рядках

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,6 +1,6 @@
-﻿Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
+﻿Console.WriteLine(" Forever in my mind");
+Console.WriteLine(" Only you");
+Console.WriteLine(" The pieces in my life go away with you");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine(" Курси Sana Commerce Software Development Bootcamp");
